d16b569b214f78ec2a605a700a0a9b1b50c6e828
rhw943359178 <943359178@qq.com>
2019-07-17 17:43:18 +0800

    2019-7-17

---
+++ day01/demo/Demo.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        //  1.字符串常量，用双引号括起来的内容
+        System.out.println("java程序员");
+
+        //  2.整数常量 所有整数
+        System.out.println(1000);
+
+        //  3.小数常量，所有小数
+        System.out.println(3.14);
+
+        //  4.字符常量，单引号括起来的，只能放单个数字，单个字母，单个符号
+        System.out.println('s');
+    }
+}
